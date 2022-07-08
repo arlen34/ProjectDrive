c6059bf0c0e61828cb3e05bb8c04052914494795
Ulukbek <ulukbekkochkorov@gmail.com>
2022-07-08 17:43:35 +0600

    1 level

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Track track1 = new Track("KG001 312AAB", "Renault Magnum");
+        Track track2 = new Track("KG001 567SAB", "Volvo");
+        Track track3 = new Track("KG003 111BAT", "DAF XT");
+        Driver driver1 = new Driver(1, "Asan");
+        Driver driver2 = new Driver(2, "Arslan");
+        Driver driver3 = new Driver(3, "Bakyt");
+
+        System.out.println();
+        System.out.println("_______________________________________________________");
+        System.out.println("# |             Track            |  Driver  |  State  |");
+        System.out.println("_______________________________________________________");
+        System.out.println("1 | " + track1 + " | " + driver1 + "     | On Base |");
+        System.out.println("2 | " + track2 + "          | " + driver2 + "   | On Base |");
+        System.out.println("3 | " + track3 + "         | " + driver3 + "    | On Base |");
     }
 }
---
+++ src/Track.java
@@ -0,0 +1,35 @@
+
+public class Track {
+    private String id;
+    private String name;
+
+    public Track(String id, String name) {
+        this.id = id;
+        this.name = name;
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public String toString() {
+        return id + '\'' +
+                name + '\'';
+    }
+}
+
